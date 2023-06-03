d2820c137f34e67baff91aaf87552a45854f0f5f
Erblin Krasniqi <erblini.kr@gmail.com>
2023-06-03 19:56:56 +0200

    Added end page cards

--- src/app/components/EndPage/EndPage.tsx
+++ src/app/components/EndPage/EndPage.tsx
@@ -9,9 +9,34 @@ const EndPage = () => {
         </h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
-          porro unde.
+          porro unde. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Quibusdam?
         </p>
-        <button></button>
+        <button>Link to GitHub</button>
+      </div>
+      <div>
+        <img src="linkedin.svg" alt="" />
+        <h2>
+          View my <a>Linkedin</a>
+        </h2>
+        <p>
+          Everything you need to build your site exactly how you imagine, from
+          automatic API handling to built-in image and performance
+          optimizations.
+        </p>
+        <button>Link to Linkedin</button>
+      </div>
+      <div>
+        <img src="email.svg" alt="" />
+        <h2>
+          View my <a>Email</a>
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+          porro unde. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Quibusdam?
+        </p>
+        <button>Link to Email</button>
       </div>
     </div>
   );
